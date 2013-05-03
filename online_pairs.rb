b6ea955dacd2a62459f6b0064eeb7b194c84d8e8
Samuel McTaggart <sam.mctaggart@gmail.com>
2013-05-03 09:00:48 -0400

    Made answer checking less specific and checked if the html parse returned an empty string or not

--- online_pairs.rb
+++ online_pairs.rb
@@ -1,6 +1,10 @@
 require 'open-uri'
 require 'nokogiri'
 
+
+#convert while loop to for loop
+#tidy up with methods and add user interactivity and score tracker to see if you've improved
+
 page = rand(24) + 1
 questions = []
 answers = []
@@ -12,11 +16,15 @@ i = 0
 
 q_a = Nokogiri::HTML(open("http://readymadepubquiz.com/quiz-#{page}-round-7-general-knowledge/"))
 q_a.xpath('//div[@class = "entry_content"]/p/strong').each do |question|
+  if question != ""
   questions.push(question.text.chomp[3..-1])
+  end
 end
 
 q_a.xpath('//div[@class = "entry_content"]/p/em').each do |answer|
+  if answer != ""
   answers.push(answer.text.chomp)
+  end
 end
 
 while count < questions.length
@@ -27,9 +35,15 @@ end
 while combined[i] != nil
   puts combined[i].keys
   answer = gets.chomp.downcase
-  if answer == combined[i].values[0].downcase
+  if answer == "" || answer.length <= 1
+    puts "You got it wrong"
+    puts "The correct answer was #{combined[i].values[0]}"
+  elsif combined[i].values[0].downcase.include?(answer)
     points += point_value
     puts "You got it right and you got a point"
+  elsif answer == "print_q_a"
+    puts combined
+    break
   else
     puts "You got it wrong"
     puts "The correct answer was #{combined[i].values[0]}"
